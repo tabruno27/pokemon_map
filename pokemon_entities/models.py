da880784f28f596541691e2fb11ac6583bac5d7b
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-04-22 16:29:23 +0200

    Снижены требования к заполненности полей в моделях Pokemon и PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,11 @@ from django.db import models  # noqa F401
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название')
-    title_ru = models.CharField(max_length=200, verbose_name='Название (русский)')
-    title_en = models.CharField(max_length=200,verbose_name='Название (английский)')
-    title_jp = models.CharField(max_length=200, verbose_name='Название (японский)')
-    photo = models.ImageField(upload_to='pokemon', default="default photo", verbose_name='Фото')
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Название')
+    title_ru = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название (русский)')
+    title_en = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название (английский)')
+    title_jp = models.CharField(max_length=200,  null=True, blank=True, verbose_name='Название (японский)')
+    photo = models.ImageField(upload_to='pokemon', default="default photo", null=True, blank=True, verbose_name='Фото')
     description = models.TextField(blank=True, verbose_name='Описание')
     previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL,
                                        null=True, blank=True, related_name='previous_evolutions', verbose_name='Предыдущая эволюция')
@@ -18,16 +18,16 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True, verbose_name='Покемон')
-    lat = models.FloatField(verbose_name='Широта')
-    lon = models.FloatField(verbose_name='Долгота')
-    appeared_at= models.DateTimeField(null=True, verbose_name='Появился в')
-    disappeared_at= models.DateTimeField(null=True, verbose_name='Исчез в')
-    level = models.IntegerField(default=1, verbose_name='Уровень')
-    health = models.IntegerField(default=100, verbose_name='Здоровье')
-    strength = models.IntegerField(default=10, verbose_name='Сила')
-    defence = models.IntegerField(default=10,verbose_name='Защита')
-    stamina = models.IntegerField(default=10, verbose_name='Выносливость')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=False, verbose_name='Покемон')
+    lat = models.FloatField(verbose_name='Широта', null=True, blank=True,)
+    lon = models.FloatField(verbose_name='Долгота', null=True, blank=True,)
+    appeared_at= models.DateTimeField(null=True, blank=True, verbose_name='Появился в')
+    disappeared_at= models.DateTimeField(null=True,blank=True, verbose_name='Исчез в')
+    level = models.IntegerField(default=1, null=True, blank=True, verbose_name='Уровень')
+    health = models.IntegerField(default=100, null=True, blank=True, verbose_name='Здоровье')
+    strength = models.IntegerField(default=10, null=True, blank=True, verbose_name='Сила')
+    defence = models.IntegerField(default=10, null=True, blank=True, verbose_name='Защита')
+    stamina = models.IntegerField(default=10, null=True, blank=True, verbose_name='Выносливость')
 
     def __str__(self):
         return f"{self.pokemon.title} at ({self.lat}, {self.lon})"
